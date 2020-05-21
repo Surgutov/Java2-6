3886bcf4e9ea4f2dfed956253c5ab912f150fbbf
surgutov <surgutovpersonal@gmail.com>
2020-05-21 20:09:05 +0300

    changed

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,5 @@
 public class BmiService {
-    public float calculate() {
-        float height = (float) 1.83;
-        float weight = (float) 60;
+    public float calculate(float height, float weight) {
         float extent = height * height;
         //возведение в квадрат, по другому не знаю как
         float imt = weight / extent;
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        float imt = service.calculate();
+        float height = (float) 1.83;
+        float weight = 61;
+        float imt = service.calculate(height, weight);
         System.out.println(imt);
     }
 }
